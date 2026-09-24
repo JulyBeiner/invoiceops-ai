401336aee2bdc50a42f377a381bba79d020f63fe
July Beiner <july.jarabeiner@gmail.com>
2026-09-24 14:37:05 +0000

    Add timestamp mixin and apply it to User

--- src/api/models/user.py
+++ src/api/models/user.py
@@ -2,9 +2,10 @@ from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
 from api.extensions import db
+from api.models.base import TimestampMixin
 
 
-class User(db.Model):
+class User(TimestampMixin, db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)
